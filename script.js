d9c4fb8087e2a38ba827f9175067daaddf465e71
bozostein <bozosteindev@gmail.com>
2024-07-27 21:31:32 +0300

    Index, Style and query js adjustments

---
+++ script.js
@@ -0,0 +1,31 @@
+const operatorButton = document.querySelector(".operators");
+const numberButton = document.querySelector(".num");
+const displayScreen = document.querySelector(".display");
+const delButton = document.querySelector("#del");
+const clearButton = document.querySelector("#clear");
+const equalButton = document.querySelector("#equal");
+const decimleButton = document.querySelector(".decimle");
+
+
+// Sum Function
+function getSum(num1, num2) {
+    return num1 - num2
+};
+// Subtract Function
+function getSubtract(num1, num2) {
+    return num1 - num2
+};
+// Multiply Function
+function getMultiply(num1, num2) {
+    return num1 * num2
+};
+// Divide Function
+function getDivide(num1, num2) {
+    return num1 / num2
+};
+
+
+
+let firstNumber;
+let secondNumber;
+let operator;
